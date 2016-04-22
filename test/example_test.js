157238dfc85a67846118fa780ec22f70b2c766a8
Mark Stahl <mjstahl@gmail.com>
2016-04-22 14:10:38 -0400

    Moved example_test to the correct directory. Added --fix to linting

--- test/example_test.js
+++ test/example_test.js
@@ -5,7 +5,6 @@ describe("Array", () => {
 	describe("#indexOf", () => {
 		it("should return -1 when value is not present", () => {
 			[1, 2, 3].indexOf(5).should.equal(-1);
-			[1, 2, 3].indexOf(0).should.equal(-1);
 		});
 	});
 });
