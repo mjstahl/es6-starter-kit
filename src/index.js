0cd294971df05209299b904c6388282302d78c28
Mark Stahl <mjstahl@gmail.com>
2016-08-25 11:48:33 -0500

    Added stage-3 preset for async/await

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import { join } from "path";
 
 join(__dirname, "...");
+
+async function asyncFun() {
+	const value = await Promise
+		.resolve(1)
+		.then(x => x * 3)
+		.then(x => x + 5)
+		.then(x => x / 2);
+	return value;
+}
+asyncFun().then(x => console.log(`x: ${x}`));
